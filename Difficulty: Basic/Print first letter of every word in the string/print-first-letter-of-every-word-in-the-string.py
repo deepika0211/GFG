54b04ec15930f038d86518b634772d5088aa6841
deepika0211 <159866900+deepika0211@users.noreply.github.com>
2025-02-23 21:34:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print first letter of every word in the string/print-first-letter-of-every-word-in-the-string.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+class Solution:
+	def firstAlphabet(self, S):
+		s=""
+		S=S.split()
+		for i in S:
+		    s+=i[0]
+		return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        S = input()
+        ob = Solution()
+        answer = ob.firstAlphabet(S)
+
+        print(answer)
+        print("~")
+
+# } Driver Code Ends
